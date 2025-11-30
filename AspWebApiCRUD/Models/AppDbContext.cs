a73fc55de6a050016c5629d08b42084a28d9f7d3
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-11-30 21:31:29 +0500

    create and upload project

---
+++ AspWebApiCRUD/Models/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspWebApiCRUD.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=IZAZ-UR-RAHMAN\\SQLEXPRESS;Database=DbForCRUD;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC079DBE7433");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AspWebApiCRUD/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspWebApiCRUD.Models;
+
+public partial class Student
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(100)]
+    public string StudentName { get; set; } = null!;
+
+    [StringLength(10)]
+    public string? Gender { get; set; }
+
+    public int Age { get; set; }
+
+    [StringLength(50)]
+    public string? Standard { get; set; }
+
+    [StringLength(100)]
+    public string? FatherName { get; set; }
+}
